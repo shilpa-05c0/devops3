e5565ef3908c87a8edf68c106fe272287525b4ed
shilpa-05c0 <lingayapallyshilpa@gmail.com>
2024-11-23 14:40:20 +0530

    first

---
+++ prime.java
@@ -0,0 +1,18 @@
+public class Prime {
+    public static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int number = 17; // Change as needed
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+}
