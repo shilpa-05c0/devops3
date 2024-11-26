2de1d5f40426f195abffda8e25696f6a8a7c6b4d
shilpa-05c0 <lingayapallyshilpa@gmail.com>
2024-11-26 10:08:39 +0530

    second

--- prime.java
+++ prime.java
@@ -1,5 +1,6 @@
 public class Prime {
     public static boolean isPrime(int n) {
+        //first commit
         if (n <= 1) return false;
         for (int i = 2; i < n; i++) {
             if (n % i == 0) return false;
